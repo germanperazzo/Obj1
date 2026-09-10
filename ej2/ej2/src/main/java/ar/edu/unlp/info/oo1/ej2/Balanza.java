87adf42aa7fea5ed32a20a98829d4330971d7328
germanperazzo <128389683+germanperazzo@users.noreply.github.com>
2026-09-10 14:46:49 -0300

    Update Balanza.java

--- ej2/ej2/src/main/java/ar/edu/unlp/info/oo1/ej2/Balanza.java
+++ ej2/ej2/src/main/java/ar/edu/unlp/info/oo1/ej2/Balanza.java
@@ -5,17 +5,24 @@ public class Balanza {
 	private int cantidadDeProductos;
 	private double precioTotal;
 	private double pesoTotal;
+	private List<Producto> productos;
+	
+	public List<Producto> getProductos(){
+		return this.productos;
+	}
 	
 	public void ponerEnCero()  {
 		this.cantidadDeProductos = 0;
 		this.precioTotal = 0;
 		this.pesoTotal =0;
+		this.productos.clear();
 	}
 	
 	public void agregarProducto(Producto p) {
 		this.cantidadDeProductos ++;
 		this.precioTotal += p.getPrecio();
 		this.pesoTotal += p.getPeso();
+		this.productos.add(new Producto(p.getDescripcion(),p.getPeso(),p.getPrecioPorKilo()));
 	}
 
 	public int getCantidadDeProductos() {
